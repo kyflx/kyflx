aaba2537bd8a9640089108597af5a4c67b248809
Chaos <leonardodimatteo94@gmail.com>
2020-02-18 17:00:26 +0100

    ok done

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,36 +1,32 @@
-import { CaseEntity, Listener } from "@vortekore/lib";
+import {CaseEntity, Listener} from "@vortekore/lib";
 import WebServer from "../../web/server";
 import DBL from "dblapi.js";
 import ms from "ms";
 
 export default class BotReady extends Listener {
-  public constructor() {
-    super("bot-ready", {
-      event: "ready",
-      emitter: "client"
-    });
-  }
-
-  async exec(client = this.client) {
-    (this.client as any).guild_manager.onReady();
-    new WebServer(client).init();
-    client.user!.setPresence({
-      activity: {
-        name: "VorteKore | v!help",
-        type: "STREAMING",
-        url: "https://twitch.tv/vortekore"
-      }
-    });
-
-    if (process.env.NODE_ENV === "production") {
-      const dbl = new DBL(client.config.get("DBL_TOKEN"), this.client);
-      setInterval(() => dbl.postStats(this.client.guilds.cache.size), 120000);
+    public constructor() {
+        super("bot-ready", {
+            event: "ready",
+            emitter: "client"
+        });
     }
 
-    [this.client.database.guilds.items].map(c =>
-      setInterval(() => c.clear(), ms("30m"))
-    );
+    async exec(client = this.client) {
+        (this.client as any).guild_manager.onReady();
+        new WebServer(client).init();
+        client.user!.setPresence({
+            activity: {
+                name: "VorteKore | v!help",
+                type: "STREAMING",
+                url: "https://twitch.tv/vortekore"
+            }
+        });
 
-    client.logger.info(`${client.user!.username} is ready to rumble!`);
-  }
+        if (process.env.NODE_ENV === "production") {
+            const dbl = new DBL(client.config.get("DBL_TOKEN"), this.client);
+            setInterval(() => dbl.postStats(this.client.guilds.cache.size), 120000);
+        }
+
+        client.logger.info(`${client.user!.username} is ready to rumble!`);
+    }
 }
