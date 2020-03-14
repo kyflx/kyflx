67911c31ecdc1d8c0bd8dd4e7c50fa707400002f
LolWastedJS <ml2dofficial@gmail.com>
2020-03-14 03:48:31 -0700

    v2 - Work on the api

--- src/web/routes/api.ts
+++ src/web/routes/api.ts
@@ -1,45 +1,13 @@
-import { MessageEmbed } from "discord.js";
 import { Request, Response } from "express";
-import { stats, logs } from "../..";
-import { APIRouter, Get, Post, Router } from "../../lib";
-import fetch from "node-fetch";
+import { APIRouter, Get, Router } from "../../lib";
 
-@Router("/api/v1")
+@Router("/api")
 export default class V1Router extends APIRouter {
   @Get()
-  public getV1(req: Request, res: Response) {
+  public getV1(_: Request, res: Response) {
     return res.status(200).json({
-      message: "VorteKore API v1"
+      message: "Welcome to the VorteKore API",
+      versions: ["v1 /api/v1"]
     });
-  }
-
-  @Get("/stats")
-  public getStats(req: Request, res: Response) {
-    return res.status(200).json(stats.register.getMetricsAsJSON());
-  }
-
-  @Post("/dbl")
-  public async dblVote(req: Request, res: Response) {
-    if (req.headers.authorization !== process.env.DBL_WEB)
-      return res.status(400);
-    if (req.body.type === "test") {
-      return this.server.logger.info("DBL Test Success!");
-    }
-
-    const user = this.client.users.resolve(req.body.user);
-    const embed = new MessageEmbed()
-      .setColor("0c6dcf")
-      .setDescription(
-        `**${
-          user ? `${user} \`(${req.body.user})\`` : req.body.user
-        }** voted for VorteKore!`
-      );
-    embed.setAuthor.apply(
-      embed,
-      user ? [req.body.user] : [user.username, user.displayAvatarURL()]
-    );
-
-    logs.send(embed);
-    return res.status(200);
   }
 }
---
+++ src/web/routes/api/v1.ts
@@ -0,0 +1,79 @@
+import { MessageEmbed } from "discord.js";
+import { Request, Response } from "express";
+import { stats, logs } from "../../..";
+import { APIRouter, Get, Post, Router } from "../../../lib";
+
+@Router("/api/v1")
+export default class V1Router extends APIRouter {
+  @Get()
+  public getV1(_: Request, res: Response) {
+    return res.status(200).json({
+      message: "Version 1 API",
+      endpoints: [
+        "GET /[top.gg|dbl]",
+        "GET /stats",
+        "POST /voted (DBL)",
+        "GET /invite",
+        "GET /[discord|server|"
+      ]
+    });
+  }
+
+  @Get("/top.gg")
+  @Get("/dbl")
+  public getDBL(req: Request, res: Response) {
+    return res.status(200).json({
+      url: "https://top.gg/bot/634766962378932224/"
+    });
+  }
+
+  @Get("/discord")
+  @Get("/server")
+  public getServer(_: Request, res: Response) {
+    return res.status(200).json({
+      invite: "https://discord.gg/8nqD2Qa"
+    });
+  }
+
+  @Get("/invite")
+  public async getInvite(_: Request, res: Response) {
+    return res.status(200).json({
+      invite: await this.client.generateInvite(["ADMINISTRATOR"])
+    });
+  }
+
+  @Get("/stats")
+  public getStats(req: Request, res: Response) {
+    return res.status(200).json({
+      prometheus: stats.register.getMetricsAsJSON(),
+      guilds: this.client.guilds.cache.size,
+      users: this.client.users.cache.size,
+      listeners: this.client.events.modules.size,
+      commands: this.client.commands.modules.filter(
+        m => m.categoryID !== "flag"
+      ).size
+    });
+  }
+
+  @Post("/voted")
+  public async dblVote(req: Request, res: Response) {
+    if (req.headers.authorization !== process.env.DBL_WEB)
+      return res.status(400);
+    if (req.body.type === "test") {
+      return this.server.logger.info("DBL Test Success!");
+    }
+
+    const user = this.client.users.resolve(req.body.user);
+    const embed = new MessageEmbed()
+      .setColor("0c6dcf")
+      .setTitle("top.gg vote")
+      .setDescription(
+        `${
+          user ? `**${user}** \`(${req.body.user})\`` : req.body.user
+        } voted for VorteKore on [top.gg](https://top.gg/bot/634766962378932224/)!`
+      );
+
+    logs.send(embed);
+    return res.status(200);
+  }
+}
