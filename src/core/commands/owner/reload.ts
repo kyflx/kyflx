f2b4e047ea23decd872f72dbd4b433593e8a6926
LolWastedJS <ml2dofficial@gmail.com>
2020-05-11 04:20:02 -0700

    Add a reload command,

---
+++ src/core/commands/owner/reload.ts
@@ -0,0 +1,41 @@
+import { Message } from "discord.js";
+import { Command, CommandOptions, Piece, Store } from "klasa";
+import { Init } from "../../../lib";
+import { Stopwatch } from "klasa";
+
+@Init<CommandOptions>({ usage: "<store:store|piece:piece|all>" })
+export default class extends Command {
+  public async run(
+    message: Message,
+    [toReload]: [Store<string, Piece> | Piece | "all"]
+  ) {
+    const stopwatch = new Stopwatch();
+    try {
+      if (toReload instanceof Store) {
+        stopwatch.start();
+        await Promise.all(toReload.map((p) => p.reload()));
+        return message.reply(
+          `Reloaded **${toReload.size}** ${
+            toReload.name
+          } in *${stopwatch.stop()}*`
+        );
+      } else if (toReload === "all") {
+        stopwatch.start();
+        await Promise.all(
+          this.client.pieceStores.map((s) => s.map((p) => p.reload()))
+        );
+        return message.reply(`Reloaded all pieces in *${stopwatch.stop()}*`);
+      } else {
+        stopwatch.start();
+        await toReload.reload();
+        return message.reply(
+          `Reloaded the **${toReload.name}** ${
+            toReload.type
+          } successfully in *${stopwatch.stop()}*`
+        );
+      }
+    } catch (error) {
+      return message.reply(`\`\`\`js\n${error}\`\`\``);
+    }
+  }
+}
