b30b29c84798d047ed92a81a0cfd38d875dd9c73
LolWastedJS <ml2dofficial@gmail.com>
2020-05-11 04:14:24 -0700

    Idk Formatting

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 import { Kyflx, KlasaConfig } from "./lib";
-
-new Kyflx(KlasaConfig).init().login(config.get("bot.token.development"));
+new Kyflx(KlasaConfig).init().login(config.getEnv("bot.token"));
